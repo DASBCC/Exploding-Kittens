f16509729a4aa29d77a99cbb7bc7738da3b9450f
Daniel Araya Sambucci <danielarayasambucci@gmail.com>
2020-07-12 21:10:26 -0600

    Avance repartir cartas

--- pruebaJuego.py
+++ pruebaJuego.py
@@ -1,5 +1,9 @@
 import random
-mazo = ["defuse", "bomba", "comodin", "favor", "shuffle", "STF"]
+mazo = ["defuse", "defuse", "defuse", "defuse", "defuse", "defuse", "bomba", "bomba", "bomba", "bomba", "comodin", "comodin", "favor", "favor", "favor", "favor", "shuffle", "shuffle", "shuffle", "shuffle", "STF", "STF", "STF", "STF", "STF"]
+mano = []
+mano2= []
+mano3= []
+mano4= []
 
 def rotaLista(x):
     x.append(x[0])
@@ -12,13 +16,47 @@ def checkIn(x,l): #revisa si x esta en l
         return False
 
 def checkBomba(l):
-    if checkIn("bomba",l)==True:
-        print ("perdiste")
+    if checkIn("bomba",l)==True and checkIn("defuse",l)==False:
+        return "Perdiste"
 
 def revuelveMazo(l):
     random.shuffle(l)
     return l
+
 def seeTheFuture(l):
     return l[:3]
 
-print(seeTheFuture(mazo))
+def tomaCarta(l):
+    mano.append(mazo[0])
+    mazo = mazo[1:]
+
+def repartidor(mazo,x):
+    mazo = revuelveMazo(mazo)
+    while x>0:
+        mano.append(mazo[0])
+        mano2.append(mazo[1])
+        mano3.append(mazo[2])
+        mano4.append(mazo[3])
+        x = x - 1
+        mazo = mazo[4:]
+    return mano, mano2, mano3, mano4, mazo
+
+def repartidorRec(mazo,x):
+    mazo = revuelveMazo(mazo)
+    if x>1:
+        mano.append(mazo[0])
+        mano2.append(mazo[1])
+        mano3.append(mazo[2])
+        mano4.append(mazo[3])
+        mazo = mazo[4:]
+        return repartidorRec(mazo,x-1)
+    else:
+        mano.append(mazo[0])
+        mano2.append(mazo[1])
+        mano3.append(mazo[2])
+        mano4.append(mazo[3])
+        x = x - 1
+        mazo = mazo[4:]
+        return mano, mano2, mano3, mano4, mazo
+
+print (repartidorRec(mazo,5))
