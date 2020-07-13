25f7627c4018ade645ae3de2102644c21c1c4ab5
kpalaciosm02 <kpalpaciosm02@gmail.com>
2020-07-13 17:00:44 -0600

    Arreglada la lista del mazo y arreglado el repartidor

--- pruebaJuego.py
+++ pruebaJuego.py
@@ -1,11 +1,11 @@
 import random
-mazo = ["defuse", "defuse", "defuse", "defuse", "defuse", "defuse", "bomba", "bomba", "bomba", "bomba", "comodin", "comodin", "favor", "favor", "favor", "favor", "shuffle", "shuffle", "shuffle", "shuffle", "STF", "STF", "STF", "STF", "STF"]
+mazo = ["defuse", "defuse", "defuse", "defuse", "defuse", "defuse", "comodin", "comodin", "comodin", "comodin", "comodin2", "comodin2", "comodin2", "comodin2", "comodin3", "comodin3", "comodin3", "comodin3", "comodin4", "comodin4", "comodin4", "comodin4", "comodin5", "comodin5", "comodin5", "comodin5", "favor", "favor", "favor", "favor", "shuffle", "shuffle", "shuffle", "shuffle", "STF", "STF", "STF", "STF", "STF"]
 mano = []
 mano2= []
 mano3= []
 mano4= []
 
-def rotaLista(x):
+def rotaLista(x):   #rota la lista 1 posición a la derecha
     x.append(x[0])
     return x[1:]
 
@@ -15,18 +15,22 @@ def checkIn(x,l): #revisa si x esta en l
     else:
         return False
 
-def checkBomba(l):
+def checkBomba(l):   #revisa si la bomba está en el mazo del jugador 
     if checkIn("bomba",l)==True and checkIn("defuse",l)==False:
         return "Perdiste"
+    else:
+        mazo.append("bomba")
+        l.remove("bomba")
+        l.remove("defuse")
 
-def revuelveMazo(l):
+def revuelveMazo(l):   #revuelve el mazo central
     random.shuffle(l)
     return l
 
-def seeTheFuture(l):
+def seeTheFuture(l):   #enseña las siguientes tres cartas en el mazo
     return l[:3]
 
-def tomaCarta(l):
+def tomaCarta(l):   #función para que un jugador coma una carta del mazo central y la ponga en su mazo
     mano.append(mazo[0])
     mazo = mazo[1:]
 
@@ -39,10 +43,12 @@ def repartidor(mazo,x):
         mano4.append(mazo[3])
         x = x - 1
         mazo = mazo[4:]
+    mazo.extend(["bomba","bomba","bomba","bomba"])
+     mazo = revuelveMazo(mazo)
     return mano, mano2, mano3, mano4, mazo
 
-def repartidorRec(mazo,x):
-    mazo = revuelveMazo(mazo)
+def repartidorRec(mazo,x):  #mazo es la lista con las cartas, x es la cantidad de cartas por jugador
+    mazo = revuelveMazo(mazo)  #pasar función a iteración 
     if x>1:
         mano.append(mazo[0])
         mano2.append(mazo[1])
@@ -59,4 +65,6 @@ def repartidorRec(mazo,x):
         mazo = mazo[4:]
         return mano, mano2, mano3, mano4, mazo
 
-print (repartidorRec(mazo,5))
+
+
+print (repartidor(mazo,7))
